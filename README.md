a896e0d76ce2585711bdcb07e62292a5a5d681e5
xm999neee <xm999neee@outlook.com>
2025-10-15 10:20:59 +0800

    b

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Modify apd/src/cli.rs:100-102: Log only errors.
 
 launchericon https://github.com/bmax121/APatch/commit/118bca9fc73d3d1403dbd9faec2e498f1fd9b547#diff-f10ece1019a63c3d819fbe608ab42b8efb524d9b86b5e1a361f1f37c7ea2deca
 
+amoled
+
 Available only in Simplified Chinese and English
 
 arm64 only
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     alias(libs.plugins.kotlin.compose.compiler)
     alias(libs.plugins.ksp)
     alias(libs.plugins.lsplugin.apksign)
+    alias(libs.plugins.lsplugin.resopt)
     alias(libs.plugins.lsplugin.cmaker)
     id("kotlin-parcelize")
 }
@@ -41,7 +42,7 @@ android {
         }
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
+            isShrinkResources = false
             isDebuggable = false
             multiDexEnabled = true
             vcsInfo.include = false
